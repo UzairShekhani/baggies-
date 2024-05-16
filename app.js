3aabfab91906a9782d1bbec659006ffb59915bac
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-16 22:58:07 +0500

    require completed

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert()
+const express = require("express")
+const app = express()
+
+const cookieParser = require("cookie-parser")
+const path = require("path")
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+app.set( "view engine", "ejs"())
+
+app.get("/", (req , res =>{
+    res.send("hello")
+}))
+
+app.listen(3000)
