3fefeb3064c00aba1f6753cb9fb3e2edaa725052
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-19 16:13:16 +0500

    userSchema

--- models/user-models.js
+++ models/user-models.js
@@ -1,3 +1,21 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("URI")
+mongoose.connect("mongodb://127.0.0.1:27017/baggie")
+
+const userSchema = mongoose.Schema({
+    fullname: String,
+    email: String,
+    password: String,
+    cart: {
+        type: Array,
+        default: []
+    },
+    Isadmin: Boolean,
+    orders: {
+        type: Array,
+        default: []
+    },
+    contact: Number,
+    picture: String
+
+})
